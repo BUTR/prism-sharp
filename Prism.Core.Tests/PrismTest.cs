2515a8181f9a8fc626784ecb51bcb80b2185a0a3
tatwd <tatwdo@gmail.com>
2022-03-17 14:44:23 +0800

    test: refactor

--- Prism.Core.Tests/PrismTest.cs
+++ Prism.Core.Tests/PrismTest.cs
@@ -81,19 +81,42 @@ public class PrismTest
             });
     }
 
-    private static void TestCase(Grammar testGrammar, string code, StringToken[] expected)
+    private static void TestCase(Grammar testGrammar, string code, Token[] expected)
     {
         var tokens = Prism.Tokenize(code, testGrammar);
-        tokens = tokens.Where(t => t.Length > 0).ToArray();
-        Assert.NotNull(tokens);
-        Assert.Equal(expected.Length, tokens.Length);
+        var simpleTokens = tokens.Where(t => !IsBlankStringToken(t)).ToArray();
+        AssertDeepStrictEqual(simpleTokens, expected);
+    }
 
-        for (var i = 0; i < expected.Length; i++)
+    private static void AssertDeepStrictEqual(IReadOnlyList<Token> simpleTokens, IReadOnlyList<Token> expected)
+    {
+        Assert.NotNull(simpleTokens);
+        Assert.Equal(expected.Count, simpleTokens.Count);
+
+        for (var i = 0; i < expected.Count; i++)
         {
-            var token = tokens[i];
-            var stringToken = Assert.IsType<StringToken>(token);
-            Assert.Equal(expected[i].Type, stringToken.Type);
-            Assert.Equal(expected[i].Content, stringToken.Content);
+            var token = simpleTokens[i];
+            var expectedToken = expected[i];
+            
+            if (expectedToken is StringToken expectedStringToken)
+            {
+                var stringToken = Assert.IsType<StringToken>(token);
+                Assert.Equal(expectedStringToken.Type, stringToken.Type);
+                Assert.Equal(expectedStringToken.Content, stringToken.Content);
+                continue;
+            }
+
+            if (expectedToken is not StreamToken expectedStreamToken) 
+                continue;
+            
+            var streamToken = Assert.IsType<StreamToken>(token);
+            AssertDeepStrictEqual(streamToken.Content, expectedStreamToken.Content);
         }
     }
+
+    private static bool IsBlankStringToken(Token token)
+    {
+        return token is StringToken stringToken && string.IsNullOrWhiteSpace(stringToken.Content);
+    }
+
 }
