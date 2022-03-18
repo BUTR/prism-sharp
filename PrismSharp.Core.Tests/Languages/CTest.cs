06820d3b5d0ffa32c37a67954a5343b117b18e6a
tatwd <tatwdo@gmail.com>
2022-03-18 20:52:03 +0800

    test(c): add cases

--- PrismSharp.Core.Tests/Languages/CTest.cs
+++ PrismSharp.Core.Tests/Languages/CTest.cs
@@ -43,4 +43,150 @@ u'\U0001f34c'";
         };
         TestHelper.RunTestCase(LanguageGrammar.C, code, expected);
     }
+
+    [Fact]
+    public void comment_test_ok()
+    {
+        const string code = @"//
+// comment
+// the comment \
+   continues!
+
+/**/
+/*
+ * comment
+ */
+
+/* open-ended comment";
+        var expected = new StringToken[]
+        {
+            new("//", "comment"),
+            new("// comment", "comment"),
+            new("// the comment \\\n   continues!", "comment"),
+            new("/**/", "comment"),
+            new("/*\n * comment\n */", "comment"),
+            new("/* open-ended comment", "comment"),
+        };
+        TestHelper.RunTestCase(LanguageGrammar.C, code, expected);
+    }
+
+    [Fact]
+    public void constant_test_ok()
+    {
+        const string code = @"__FILE__
+__LINE__
+__DATE__
+__TIME__
+__TIMESTAMP__
+__func__
+EOF
+NULL
+SEEK_CUR
+SEEK_END
+SEEK_SET
+stdin
+stdout
+stderr";
+        var expected = new StringToken[]
+        {
+            new("__FILE__", "constant"),
+            new("__LINE__", "constant"),
+            new("__DATE__", "constant"),
+            new("__TIME__", "constant"),
+            new("__TIMESTAMP__", "constant"),
+            new("__func__", "constant"),
+            new("EOF", "constant"),
+            new("NULL", "constant"),
+            new("SEEK_CUR", "constant"),
+            new("SEEK_END", "constant"),
+            new("SEEK_SET", "constant"),
+            new("stdin", "constant"),
+            new("stdout", "constant"),
+            new("stderr", "constant"),
+        };
+        TestHelper.RunTestCase(LanguageGrammar.C, code, expected);
+    }
+
+    [Fact]
+    public void function_test_ok()
+    {
+        const string code = @"foo(void);
+bar
+(1, 2);";
+        var expected = new StringToken[]
+        {
+            new("foo", "function"),
+            new("(", "punctuation"),
+            new("void", "keyword"),
+            new(")", "punctuation"),
+            new(";", "punctuation"),
+            new("bar", "function"),
+            new("(", "punctuation"),
+            new("1", "number"),
+            new(",", "punctuation"),
+            new("2", "number"),
+            new(")", "punctuation"),
+            new(";", "punctuation"),
+        };
+        TestHelper.RunTestCase(LanguageGrammar.C, code, expected);
+    }
+
+    [Fact]
+    public void class_name_test_ok()
+    {
+        const string code = @"struct foo;
+enum bar;
+
+struct foo var;
+struct __attribute__ ((aligned (8))) S { short f[3]; };
+
+// by name
+uint32_t foo;
+static dtrace_helptrace_t *bar;";
+        var expected = new StringToken[]
+        {
+            new("struct", "keyword"),
+            new("foo", "class-name"),
+            new(";", "punctuation"),
+            new("enum", "keyword"),
+            new("bar", "class-name"),
+            new(";", "punctuation"),
+
+            new("struct", "keyword"),
+            new("foo", "class-name"),
+            new(" var"),
+            new(";", "punctuation"),
+            new("struct", "keyword"),
+            new("__attribute__", "keyword"),
+            new("(", "punctuation"),
+            new("(", "punctuation"),
+            new("aligned", "function"),
+            new("(", "punctuation"),
+            new("8", "number"),
+            new(")", "punctuation"),
+            new(")", "punctuation"),
+            new(")", "punctuation"),
+            new("S", "class-name"),
+            new("{", "punctuation"),
+            new("short", "keyword"),
+            new(" f"),
+            new("[", "punctuation"),
+            new("3", "number"),
+            new("]", "punctuation"),
+            new(";", "punctuation"),
+            new("}", "punctuation"),
+            new(";", "punctuation"),
+
+            new("// by name", "comment"),
+            new("uint32_t", "class-name"),
+            new(" foo"),
+            new(";", "punctuation"),
+            new("static", "keyword"),
+            new("dtrace_helptrace_t", "class-name"),
+            new("*", "operator"),
+            new("bar"),
+            new(";", "punctuation"),
+        };
+        TestHelper.RunTestCase(LanguageGrammar.C, code, expected);
+    }
 }
