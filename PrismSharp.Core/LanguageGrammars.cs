2cc52b39502c3511608e927dd76f01c6c197394b
tatwd <tatwdo@gmail.com>
2022-06-15 20:38:21 +0800

    refactor

--- PrismSharp.Core/LanguageGrammars.cs
+++ PrismSharp.Core/LanguageGrammars.cs
@@ -5,44 +5,65 @@ namespace PrismSharp.Core;
 // TODO: maybe the class could be created by `source-generators`
 public static class LanguageGrammars
 {
-    private static readonly IReadOnlyDictionary<string, Lazy<Grammar>> Definitions;
+    private static readonly IDictionary<string, Lazy<Grammar>> Definitions;
 
     static LanguageGrammars()
     {
-        Definitions = new Dictionary<string, Lazy<Grammar>>
-        {
-            ["csharp"] = new(() => new CSharp().Define()),
-            ["javascript"] = new(() => new JavaScript().Define()),
-        };
+        Definitions = new Dictionary<string, Lazy<Grammar>>(16);
+
+        AddDefinition<C>("c");
+        AddDefinition<CLike>("clike");
+        AddDefinition<CSharp>("csharp", "cs", "dotnet");
+        AddDefinition<AspNet>("aspnet", "aspx");
+        AddDefinition<JavaScript>("javascript", "js");
+        AddDefinition<RegExp>("regexp", "regex");
+        AddDefinition<Markup>("markup", "html", "mathml", "svg", "xml", "atom", "rss");
+        AddDefinition<Sql>("sql");
+        AddDefinition<Json>("json", "web-manifest");
+        AddDefinition<PowerShell>("powershell", "ps1");
+        AddDefinition<Yaml>("yaml", "yml");
+        AddDefinition<Css>("css");
+        AddDefinition<Lua>("lua");
     }
 
+    public static void AddDefinition<T>(params string[] alias) where T : IGrammarDefinition, new()
+    {
+        var lazyVal = new Lazy<Grammar>(() => new T().Define());
+        foreach (var name in alias)
+            Definitions.Add(name, lazyVal);
+    }
 
-    public static Grammar C => new C().Define();
-    public static Grammar Cpp => Definitions["cpp"].Value;
-    public static Grammar CLike => new CLike().Define();
-    public static Grammar CSharp => Definitions["csharp"].Value;
-    public static Grammar Cs => CSharp;
-    public static Grammar DotNet => CSharp;
-    public static Grammar AspNet => new AspNet().Define();
-    public static Grammar Aspx => AspNet;
-    public static Grammar JavaScript => Definitions["javascript"].Value;
-    public static Grammar Js => JavaScript;
-    public static Grammar RegExp => new RegExp().Define();
-    public static Grammar Regex => RegExp;
-    public static Grammar Markup => new Markup().Define();
-    public static Grammar Html => Markup;
-    public static Grammar Mathml => Markup;
-    public static Grammar Svg => Markup;
-    public static Grammar Xml => Markup;
-    public static Grammar Atom => Xml;
-    public static Grammar Rss => Xml;
-    public static Grammar Sql => new Sql().Define();
-    public static Grammar Json => new Json().Define();
-    public static Grammar WebManifest => Json;
-    public static Grammar PowerShell = new PowerShell().Define();
-    public static Grammar Ps1 => PowerShell;
-    public static Grammar Yaml => new Yaml().Define();
-    public static Grammar Yml => Yaml;
-    public static Grammar Css => new Css().Define();
-    public static Grammar Lua => new Lua().Define();
+    public static Grammar GetGrammar(string language)
+    {
+        Definitions.TryGetValue(language, out var grammar);
+        return grammar?.Value ?? new Grammar();
+    }
+
+    public static Grammar C => GetGrammar("c");
+    public static Grammar CLike => GetGrammar("clike");
+    public static Grammar CSharp => GetGrammar("csharp");
+    // public static Grammar Cs => CSharp;
+    // public static Grammar DotNet => CSharp;
+    public static Grammar AspNet => GetGrammar("aspnet");
+    // public static Grammar Aspx => AspNet;
+    public static Grammar JavaScript => GetGrammar("javascript");
+    // public static Grammar Js => JavaScript;
+    public static Grammar RegExp => GetGrammar("regexp");
+    // public static Grammar Regex => RegExp;
+    public static Grammar Markup => GetGrammar("markup");
+    // public static Grammar Html => Markup;
+    // public static Grammar Mathml => Markup;
+    // public static Grammar Svg => Markup;
+    // public static Grammar Xml => Markup;
+    // public static Grammar Atom => Xml;
+    // public static Grammar Rss => Xml;
+    public static Grammar Sql => GetGrammar("sql");
+    public static Grammar Json => GetGrammar("json");
+    // public static Grammar WebManifest => Json;
+    public static Grammar PowerShell => GetGrammar("powershell");
+    // public static Grammar Ps1 => PowerShell;
+    public static Grammar Yaml => GetGrammar("yaml");
+    // public static Grammar Yml => Yaml;
+    public static Grammar Css => GetGrammar("css");
+    public static Grammar Lua => GetGrammar("lua");
 }
